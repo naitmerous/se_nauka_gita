e9cf764ad3757f24585a91738c5c20bd60147c52
naitmerous <fejbian21@gmail.com>
2025-03-08 11:27:14 +0100

    srednia wazona

--- avg1.php
+++ avg1.php
@@ -3,4 +3,13 @@ function zwykla_srednia(a,b)
 {
 	return (a+b)/2;
 }
+function srednia_wazona($a = 0, $b = 0)
+{
+	$wynik = ($a+b)/2;
+	return $wynik;
+}
+$a = 8;
+$b = 6;
+$wynik = srednia_wazona($a,$b);
+echo ($wynik);
 ?>
